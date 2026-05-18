339c06421c9d76ccbaebad0104a07f34740c1d27
nickkostov <35745114+nickkostov@users.noreply.github.com>
2026-05-18 16:27:47 +0300

    feat(pkg): Installation script

---
+++ wsl/install/libs/helper.sh
@@ -0,0 +1,10 @@
+#!/bin/env bash
+set -euo pipefail
+
+install() {
+  echo "Installing software and tools for normal operations"
+}
+
+noinstall() {
+  echo "No installation needed, all software and tools are already installed"
+}
